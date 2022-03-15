077bc20e749e2e940acfbfae9caa05d17e091ee7
Hugo Passos <hugopassos@outlook.com>
2022-03-15 13:33:38 -0300

    Exercícios 1, 2 , 3, 4 e 5

--- 01 - fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios.js
+++ 01 - fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios.js
@@ -3,6 +3,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Exercicio 1:
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+
 
 // com o laço de repeticão "for"
 // for (let i = 0; i < numbers.length; i += 1) {
@@ -15,22 +17,50 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 
 // Exercicio 2:
-let sum = 0;
-for (let i = 0; i < numbers.length; i += 1) {
-    sum = sum + numbers[i]; // recebe os valores do array em cada laço de repetição somando com a soma anterior.
-}
-console.log(sum);
+// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i += 1) {
+//     sum = sum + numbers[i]; // recebe os valores do array em cada laço de repetição somando com a soma anterior.
+// }
+// console.log(sum);
 
 // Exercicio 3:
+// Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 
-
+// let sum = 0;
+// let avg = 0;
+// for (let i = 0; i < numbers.length; i += 1) {
+//     sum = sum + numbers[i]; // recebe os valores do array em cada laço de repetição somando com a soma anterior.
+//     avg = sum/(i+1);
+// }
+// console.log(avg);
 
 // Exercicio 4:
+// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
+// let sum = 0;
+// let avg = 0;
+// for (let i = 0; i < numbers.length; i += 1) {
+//     sum = sum + numbers[i]; // recebe os valores do array em cada laço de repetição somando com a soma anterior.
+//     avg = sum / (i + 1);
+// }
+// if (avg > 20) {
+//     console.log('valor maior que 20');
+// } else {
+//     console.log('valor menor que 20');
+// }
 
 
 // Exercicio 5:
-
+// Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+let numberHigh = 0;
+for (let i = 0; i < numbers.length; i += 1) {
+    if (numberHigh <= numbers[i]) {
+        numberHigh = numbers[i];
+    }
+}
+    console.log(numberHigh);
 
 
 // Exercicio 6:
