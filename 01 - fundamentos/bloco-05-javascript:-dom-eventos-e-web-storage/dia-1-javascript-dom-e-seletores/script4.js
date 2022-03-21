936d7b7ad15fc66a82f005cad3a9ba4f852c1a1a
Hugo Passos <hugopassos@outlook.com>
2022-03-21 00:29:44 -0300

    Finalizando os  exercicios dia 5.1

--- 01 - fundamentos/bloco-05-javascript:-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script4.js
+++ 01 - fundamentos/bloco-05-javascript:-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script4.js
@@ -6,10 +6,6 @@ document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = 'pur
 document.querySelector('.emergency-tasks').style.backgroundColor = 'pink';
 document.querySelector('.no-emergency-tasks').style.backgroundColor = 'yellow';
 
-
-// document.getElementsByTagName('section')[0].style.backgroundColor = 'rose';
-// document.querySelectorAll('.emergency-tasks')[1].style.backgroundColor = 'rose';
-
 document.querySelectorAll('.no-emergency-tasks h3')[0].style.backgroundColor = 'black';
 document.querySelectorAll('.no-emergency-tasks h3')[1].style.backgroundColor = 'black';
 
