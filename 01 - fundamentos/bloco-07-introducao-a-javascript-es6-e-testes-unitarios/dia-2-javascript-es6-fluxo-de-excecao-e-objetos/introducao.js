2da6ab890906e2a5bcd653c2b77f48e33ec068a5
Hugo Passos <hugopassos@outlook.com>
2022-04-04 14:01:02 -0300

    Iniciando estudos dia 7.2

---
+++ 01 - fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/introducao.js
@@ -0,0 +1,26 @@
+// O que vamos aprender?
+
+// Hoje você vai aprender um pouco sobre fluxo de exceção! Ou seja, como tratar os erros do seu código utilizando os métodos throw e try/catch . Além disso, você verá algumas funções que interagem com objetos . Esses métodos te ajudarão bastante quando for necessária alguma leitura de seus valores ou chaves, ou quando precisar copiá-los. Esse é um passo muito importante para o bom e mais avançado uso do JavaScript . Objetos são muito presentes em todas as lógicas do ES6 . Os métodos que veremos são:
+// Object.keys ;
+// Object.entries ;
+// Object.assign ;
+// Object.values .
+
+// Você será capaz de:
+
+// Lançar erros com throw e capturá-los com o bloco try/catch ;
+// Acessar todas as chaves de um objeto com o método Object.keys ;
+// Acessar os pares chave-valor de um objeto com o método Object.entries ;
+// Acessar todos os valores de um objeto com o método Object.values ;
+// Copiar um objeto com o método Object.assign ;
+// Adicionar propriedades a um objeto.
+
+// Por que isso é importante?
+
+// Em JavaScript, o tratamento de erros em um código é fundamental, pois ajudará você a entender melhor o que está ocasionando tais erros. Além disso, aprender a lidar com objetos e suas propriedades é essencial, uma vez que você se deparará com eles frequentemente. Você verá métodos que servem para melhorar o uso de um objeto, facilitando o acesso a suas informações e te permitindo adicionar novas funcionalidades a ele. Um exemplo prático disso é que você não precisará mais utilizar um for para percorrer um objeto para pegar sua lista de chaves ou de valores.
+
+// Conteúdos
+
+// Até aqui, você já aprendeu a criar funções e obter os mais diversos resultados com elas, mas e quando as coisas dão errado? Como tratar os erros? Hoje você verá como dar maior legibilidade a esses erros e, consequentemente, ao seu código como um todo.
+// Você também viu e utilizou bastante os objetos no JavaScript e hoje aprenderá sobre métodos com objetos bastante úteis para a lógica do seu código. No decorrer do conteúdo, execute os exemplos na sua máquina para ver melhor como cada método funciona. Sinta-se livre para mudar os objetos ou o código para explorar mais cada exemplo.
+
